4dd18d5961f8c6f8da7c65b913661349f6a53ab5
Gustavo Alves <gustavo.acinfo@gmail.com>
2021-11-26 23:09:39 -0300

    Project created

---
+++ orderarray/src/main/java/com/crosscomerce/orderarray/OrderarrayApplication.java
@@ -0,0 +1,13 @@
+package com.crosscomerce.orderarray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderarrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderarrayApplication.class, args);
+	}
+
+}
---
+++ orderarray/src/test/java/com/crosscomerce/orderarray/OrderarrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crosscomerce.orderarray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderarrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
