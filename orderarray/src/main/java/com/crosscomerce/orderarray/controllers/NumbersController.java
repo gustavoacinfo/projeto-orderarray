09dd39f8512c8445527ce4959e525e1851fa5187
Gustavo Alves <gustavo.acinfo@gmail.com>
2021-11-29 19:37:18 -0300

    Methods of grouping and ordering

--- orderarray/src/main/java/com/crosscomerce/orderarray/controllers/NumbersController.java
+++ orderarray/src/main/java/com/crosscomerce/orderarray/controllers/NumbersController.java
@@ -1,13 +1,12 @@
 package com.crosscomerce.orderarray.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.crosscomerce.orderarray.models.Numbers;
 import com.crosscomerce.orderarray.services.NumbersService;
 
 @RestController
@@ -17,12 +16,17 @@ public class NumbersController {
 	@Autowired
 	private NumbersService numberService;
 	
-	@GetMapping("/{numpage}")
-	public ResponseEntity<Numbers> getNumbers(@PathVariable Integer numpage){
+	@GetMapping("ordered")
+	public ResponseEntity<ArrayList<Double>> getNumbersOrdered(){ 
 		
-		Numbers number = this.numberService.getNumbers(numpage);
+		return ResponseEntity.ok(this.numberService.orderArray());
 		
-		return ResponseEntity.ok(number);
+	}
+	
+	@GetMapping("unordered")
+	public ResponseEntity<ArrayList<Double>> getNumbersUnordered(){ 
+		
+		return ResponseEntity.ok(this.numberService.getNumbers(1));
 		
 	}
 }
--- orderarray/src/main/java/com/crosscomerce/orderarray/services/NumbersService.java
+++ orderarray/src/main/java/com/crosscomerce/orderarray/services/NumbersService.java
@@ -1,5 +1,7 @@
 package com.crosscomerce.orderarray.services;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,28 @@ public class NumbersService {
 	@Autowired
 	private WebClient webClient;
 	
-	public Numbers getNumbers(Integer numPage) {
+	public ArrayList<Double> orderArray() { 
+		
+		ArrayList<Double> orderArray = new ArrayList<>(this.getNumbers(1));
+		
+		
+		for (int i = 0; i < orderArray.size(); i++) {
+	        for (int j = i + 1; j < orderArray.size(); j++) {
+	            Double x = 0.0;
+	            if (orderArray.get(i) > orderArray.get(j)) {
+	                x = orderArray.get(i);
+	                orderArray.set(i, orderArray.get(j)); 
+	                orderArray.set(j, x);
+	            }
+	        }
+	    }
+		
+		return orderArray;
+	
+	}
+	
+	
+	public ArrayList<Double> getNumbers(Integer numPage) {
 		
 		Mono<Numbers> monoNumbers = this.webClient
 			.method(HttpMethod.GET)
@@ -22,9 +45,14 @@ public class NumbersService {
 			.retrieve()
 			.bodyToMono(Numbers.class);
 		
-		Numbers number = monoNumbers.block();
+		Numbers numbers = monoNumbers.block();
+		ArrayList<Double> allNumbers = new ArrayList<>(numbers.getNumbers());
 		
-		return number;
+		if (numbers.getNumbers().size() > 0 && numPage <= 2) {
+			allNumbers.addAll(this.getNumbers(++numPage));
+		}
+		
+		return allNumbers;
 	}
 
 }
